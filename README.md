b4fef437566010171b223407f3cb527012092ab2
Romain Pellerin <rpellerin@users.noreply.github.com>
2022-09-24 15:14:59 +0200

    Minor edits

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Browse safely.
 
 ```shell
 npm install
-./index.js --update
-# Check you hosts file contains only lines redirecting to localhost
-cat hosts_files/* | grep -E '^(127.0.0.1|\s*#|0.0.0.0)' -v | grep -v '^$'
-./index.js --protect-me
+./index.js --update # Copies locally the remote hosts files defined in `list.json`
+./index.js --protect-me # Concatenates these copies in /etc/hosts
 ```
--- scripts/protect-me.sh
+++ scripts/protect-me.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
 
+CURRENT_DATETIME="$(date --iso-8601=seconds)"
+
 if [ ! -f /etc/hosts.initial.backup ]; then
     echo "No initial backup found! Creating one..."
-    sudo cp /etc/hosts /etc/hosts.initial.backup
+    sudo cp /etc/hosts "/etc/hosts.initial.backup"
     echo "Backup file created."
 fi
 
-sudo cp /etc/hosts /etc/hosts.`date --iso-8601=seconds`.backup
-cat /etc/hosts.initial.backup $@ | tr -d '\r' | grep -v whatsapp | sudo tee /etc/hosts.tmp >/dev/null
+sudo cp /etc/hosts "/etc/hosts.$CURRENT_DATETIME.backup"
+cat /etc/hosts.initial.backup <(cat $@ | grep -E '^(127.0.0.1|\s*#|0.0.0.0)') | tr -d '\r' | sudo tee /etc/hosts.tmp >/dev/null
 sudo mv /etc/hosts.tmp /etc/hosts -f
  
